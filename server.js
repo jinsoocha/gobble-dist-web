861d45638be7477200f7d314396b94d68a9324a6
Jack Zhang <15jzhang@gmail.com>
2016-06-22 19:44:32 -0700

    Add bare express server with dotenv environment variables

---
+++ server.js
@@ -0,0 +1,22 @@
+require('babel-polyfill');
+
+// Load environment variables
+const dotenv = require('dotenv');
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config({ path: './env/development.env' });
+} else if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: './env/production.env' });
+}
+
+const express = require('express');
+const app = express();
+
+app.get('/*', () => {
+  res.send('It works!');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`${process.env.APP_NAME} is listening on port ${process.env.PORT}.`);
+});
+
+module.exports = app;
