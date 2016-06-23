5795ec3ea522c0dd6593db062aac48d5a608333a
Jack Zhang <15jzhang@gmail.com>
2016-06-23 16:28:13 -0700

    Implement Passport Facebook Strategy Configuration
    Persisting of user data and serialization still to be implemented.
    These features require communication with the gobble-db
    microservice.

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,6 +1,5 @@
 import session from 'express-session';
 import { Strategy as FacebookStrategy } from 'passport-facebook';
-// var User = require('../models/user');
 
 const configAuth = (app, passport) => {
   // Express session middleware
@@ -15,6 +14,59 @@ const configAuth = (app, passport) => {
   // Initialize passport authentication & persistent sessions
   app.use(passport.initialize());
   app.use(passport.session());
+
+  passport.use(
+    new FacebookStrategy({
+      // Register the application using a Facebook Developer Account
+      clientID: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      callbackURL: `${process.env.PROTOCOL}${process.env.HOST}:${process.env.PORT}/auth/facebook/callback`,
+      enableProof: true,
+      profileFields: ['id', 'displayName', 'gender', 'picture.type(large)', 'emails']
+    },
+    (accessToken, refreshToken, profile, done) => {
+      process.nextTick(() => {
+        // Implement through gobble-db interface
+        // User.where({ facebookId: profile.id }).fetch()
+        //   .then((user) => {
+        //     // Persist user if not found
+        //     if (!user) {
+        //       return new User({
+        //         name: profile.displayName,
+        //         facebookId: profile.id,
+        //         picture: profile.photos[0].value
+        //       }).save();
+        //     }
+        //     return user;
+        //   })
+        //   .then((user) => {
+        //     done(null, user);
+        //   })
+        //   .catch((err) => {
+        //     done(err, null);
+        //   });
+        done(null, profile);
+      });
+    }
+  ));
+
+  // Passport session serialization setup
+  passport.serializeUser((user, done) => {
+    done(null, user.get('facebookId'));
+  });
+
+  passport.deserializeUser((facebookId, done) => {
+    // Implement through gobble-db interface
+    // User.where({ facebookId: facebookId }).fetch()
+    //   .then((user) => {
+    //     done(null, user);
+    //   })
+    //   .catch((err) => {
+    //     done(err, null);
+    //   });
+
+    done(null, user);
+  });
 };
 
 export default configAuth;
