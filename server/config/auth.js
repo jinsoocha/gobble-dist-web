282dee4ff889e2e0cd39b8a134e3404e2b4cefb9
Jack Zhang <15jzhang@gmail.com>
2016-06-30 17:58:45 -0700

    Implement Authentication logic for persisting new users, add fetch helpers

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,5 +1,37 @@
 import session from 'express-session';
+import { checkStatus, parseJSON, handleError } from './../lib/fetch-utils';
 import { Strategy as FacebookStrategy } from 'passport-facebook';
+
+import fetch from 'isomorphic-fetch';
+
+const createNewUser = (profile) => {
+  console.log('GOT HERE', profile);
+
+  // const newUser = {
+  //   facebook_id: req.body.facebook_id,
+  //   first_name: req.body.first_name,
+  //   last_name: req.body.last_name,
+  //   display_name: req.body.display_name,
+  //   gender: req.body.gender,
+  //   photo_url: req.body.photo_url,
+  // };
+
+  return fetch(`${process.env.GOBBLE_API_URL}/user`, {
+    method: 'POST',
+    body: JSON.stringify(newUser),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(checkStatus)
+    .then(parseJSON)
+    .then(user => {
+      return user;
+    })
+    .catch(err => {
+      handleError(err);
+      res.sendStatus(err.res.status);
+    });
+};
 
 const configAuth = (app, passport) => {
   // Express session middleware
@@ -22,11 +54,31 @@ const configAuth = (app, passport) => {
       clientSecret: process.env.FACEBOOK_SECRET,
       callbackURL: `${process.env.PROTOCOL}${process.env.HOST}:${process.env.PORT}/auth/facebook/callback`,
       enableProof: true,
-      profileFields: ['id', 'name', 'profileUrl', 'displayName', 'gender', 'picture.type(large)', 'emails']
+      profileFields: ['id', 'name', 'email', 'displayName', 'gender', 'picture.type(large)']
     },
     (accessToken, refreshToken, profile, done) => {
+      // console.log(profile);
       process.nextTick(() => {
-        // Implement through gobble-db interface
+        fetch(`${process.env.GOBBLE_API_URL}/user?facebook_id=${profile.id}`)
+          .then(checkStatus)
+          .then(parseJSON)
+          .then(res => {
+            if (res.status === 200) {
+              console.log('Existing user authenticated.');
+              console.log(res);
+            } else if (res.status === 404) {
+              // User is new - persist the new user
+              createNewUser(profile)
+                .then(user => {
+                  console.log('New user successfully persisted:', user);
+                  done(null, user);
+                });
+            } else {
+              handleError(err);
+              res.sendStatus(err.res.status);
+            }
+          });
+      });
         // User.where({ facebookId: profile.id }).fetch()
         //   .then((user) => {
         //     // Persist user if not found
@@ -45,8 +97,6 @@ const configAuth = (app, passport) => {
         //   .catch((err) => {
         //     done(err, null);
         //   });
-        done(null, profile);
-      });
     }
   ));
 
---
+++ server/lib/fetch-utils.js
@@ -0,0 +1,22 @@
+const checkStatus = res => {
+  if (res.status >= 200 && res.status < 300) {
+    return res;
+  } else {
+    const error = new Error(res.statusText);
+    error.res = res;
+    throw error;
+  }
+};
+
+const parseJSON = res => res.json();
+
+const handleError = err => {
+  console.error(`ERROR - Request to ${err.res.url} failed:
+    ${err.res.status} ${err.res.statusText}`);
+};
+
+export default {
+  checkStatus,
+  parseJSON,
+  handleError
+};
