e34970a26caec052a882d49ec539649021ea541c
Jack Zhang <15jzhang@gmail.com>
2016-07-01 01:12:59 -0700

    Add custom profile image and user first name to nav bar

--- common/main-layout/NavBarAuthContainer.js
+++ common/main-layout/NavBarAuthContainer.js
@@ -1,10 +1,11 @@
-import connect from 'react-redux';
+import { connect } from 'react-redux';
+
 import NavBarAuth from './NavBarAuth';
 
 const mapStateToProps = state => ({
-  facebookId: state.layout.navBarAuth.facebook_id,
-  firstName: state.layout.navBarAuth.firstName,
-  photoUrl: state.layout.navBarAuth.photo_url
+  facebookId: state.layout.navBarUser.facebookId.toString(),
+  firstName: state.layout.navBarUser.firstName,
+  photoUrl: state.layout.navBarUser.photoUrl
 });
 
 export default connect(mapStateToProps)(NavBarAuth);
