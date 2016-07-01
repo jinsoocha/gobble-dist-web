0c87dc2db1b8e86795e9dd831afe8196ca505a18
Leo Adelstein <leoadelstein@gmail.com>
2016-06-30 20:43:00 -0700

    added file uploading for a review

---
+++ common/user-app/add-post/FileUpload.js
@@ -0,0 +1,33 @@
+const React = require('react');
+
+class FileUpload extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+    const Dropzone = window.Dropzone;
+    const dropzone = new Dropzone('#dropzone', { url: 'http://localhost:3003/api/media' });
+    dropzone.on('success', (err, res) => {
+      console.log(`image name: ${res}`);
+      this.props.handleFile(res);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="dropzone" className="dropzone"> </div>
+      </div>
+    );
+  }
+}
+
+FileUpload.propTypes = {
+  handleFile: React.PropTypes.func,
+};
+
+export default FileUpload;
