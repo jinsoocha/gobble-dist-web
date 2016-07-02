11d316a14659b553a996087cd6b1c90ca4bdd2dc
Jack Zhang <15jzhang@gmail.com>
2016-07-01 20:27:43 -0700

    Control modal and overlay display and styles

--- common/main-layout/NavBarAuth.js
+++ common/main-layout/NavBarAuth.js
@@ -1,55 +1,49 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import SearchBarContainer from './SearchBarContainer';
 import Modal from 'react-modal';
 import AddPost from './../shared/add-post/AddPost';
 
-class NavBarAuth extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { facebookId, firstName, photoUrl, modalIsOpen, openModal, closeModal } = this.props;
-    return (
-      <div className="navbar-auth navbar-wrapper">
-        <div className="navbar-menu pure-menu pure-menu-horizontal">
-          <a href="/" className="navbar-menu-heading pure-menu-heading">LOGO</a>
-          <SearchBarContainer />
-          <ul className="navbar-menu-list pure-menu-list">
-            <li className="pure-menu-item">
-              <a href={`/${facebookId}`} className="navbar-menu-link pure-menu-link">
-                <img className="navbar-profile-photo" src={photoUrl} alt="Facebook Profile" />
-                {firstName}
-              </a>
-            </li>
-            <li className="pure-menu-item"><a href="/" className="navbar-menu-link pure-menu-link">Home</a></li>
-            <li className="pure-menu-item">
-              <span className="gobble-modal-link navbar-menu-link pure-menu-link" onClick={openModal}>
-                <i className="gobble-modal-link-icon fa fa-cutlery" aria-hidden="true" />
-                Gobble
-                <Modal
-                  className="add-post-modal"
-                  overlayClassName="add-post-modal-overlay"
-                  isOpen={modalIsOpen}
-                  onRequestClose={closeModal}
-                >
-                  <AddPost />
-                </Modal>
-              </span>
-            </li>
-            <li className="navbar-dropdown pure-menu-item pure-menu-has-children pure-menu-allow-hover">
-              <div className="pure-menu-link"><i className="fa fa-cog" aria-hidden="true" /></div>
-              <ul className="pure-menu-children">
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link dropdown-link">Settings</a></li>
-                <li className="pure-menu-item"><a href="/logout" className="pure-menu-link dropdown-link">Sign out</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+const NavBarAuth = (props) => {
+  const { facebookId, firstName, photoUrl, modalIsOpen, openModal, closeModal } = props;
+  return (
+    <div className="navbar-auth navbar-wrapper">
+      <div className="navbar-menu pure-menu pure-menu-horizontal">
+        <a href="/" className="navbar-menu-heading pure-menu-heading">LOGO</a>
+        <SearchBarContainer />
+        <ul className="navbar-menu-list pure-menu-list">
+          <li className="pure-menu-item">
+            <a href={`/${facebookId}`} className="navbar-menu-link pure-menu-link">
+              <img className="navbar-profile-photo" src={photoUrl} alt="Facebook Profile" />
+              {firstName}
+            </a>
+          </li>
+          <li className="pure-menu-item"><a href="/" className="navbar-menu-link pure-menu-link">Home</a></li>
+          <li className="pure-menu-item">
+            <span className="gobble-modal-link navbar-menu-link pure-menu-link" onClick={openModal}>
+              <i className="gobble-modal-link-icon fa fa-cutlery" aria-hidden="true" />
+              Gobble
+              <Modal
+                className="add-post-modal"
+                overlayClassName="add-post-modal-overlay"
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+              >
+                <AddPost />
+              </Modal>
+            </span>
+          </li>
+          <li className="navbar-dropdown pure-menu-item pure-menu-has-children pure-menu-allow-hover">
+            <div className="pure-menu-link"><i className="fa fa-cog" aria-hidden="true" /></div>
+            <ul className="pure-menu-children">
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link dropdown-link">Settings</a></li>
+              <li className="pure-menu-item"><a href="/logout" className="pure-menu-link dropdown-link">Sign out</a></li>
+            </ul>
+          </li>
+        </ul>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 NavBarAuth.propTypes = {
   facebookId: PropTypes.string.isRequired,
