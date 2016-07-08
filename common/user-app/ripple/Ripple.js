1da722d8c72ac8177bef81df4941a30c52cfea9c
Leo Adelstein <leoadelstein@gmail.com>
2016-07-07 17:32:07 -0700

    added ripple feed to ripple page

--- common/user-app/ripple/Ripple.js
+++ common/user-app/ripple/Ripple.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import RippleMap from './RippleMap.js';
+import AddPostContainer from './../../shared/add-post/AddPostContainer';
+import Posts from '../../shared/post/PostContainer';
 
 const Ripple = () => (
   <div className="ripple-view">
-    <h1>Ripple Here</h1>
-    <RippleMap />
+    <div className="pure-g">
+      <div className="pure-u-12-24">
+        <RippleMap />
+      </div>
+      <div className="pure-u-8-24">
+        <AddPostContainer />
+        <Posts type="ripple" />
+      </div>
+    </div>
   </div>
 );
 
